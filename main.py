0e4e6e31ac17f4d477879a549832d6477566aadc
NikoChristie <57568230+NikoChristie@users.noreply.github.com>
2020-11-18 11:28:37 -0600

    Changed or in if statement to tuple

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ file = open('words.txt')
 
 for i in file.readlines():
     s = i.strip()
-    if s.endswith('er') or i.endswith('or') or i.endswith('ur'):
+    if s.endswith(('er', 'or', 'ur')):
         print("{0} her? I barley even knew her!".format(s[:-2]))
 
 file.close()
