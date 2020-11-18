bb35a88d05b67bed661355a2755e5d9d7aeeb9f1
NikoChristie <57568230+NikoChristie@users.noreply.github.com>
2020-11-18 11:34:26 -0600

    Added new er end sounds

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ file = open('words.txt')
 
 for i in file.readlines():
     s = i.strip()
-    if s.endswith(('er', 'or', 'ur')):
+    if s.endswith(('er', 'or', 'ur', 'ear', 'ire')):
         print("{0} her? I barley even knew her!".format(s[:-2]))
 
 file.close()
